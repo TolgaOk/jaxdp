775784c73ab7cb063fa943e634799a66ce4905c2
Tolga Ok <tolgaokk@gmail.com>
2024-01-23 21:04:00 +0100

    Update learning: OOP to functional

--- jaxdp/learning/learning.py
+++ jaxdp/learning/learning.py
@@ -1,67 +1,102 @@
-from typing import Dict, Union, List, NamedTuple
+from typing import Callable, Dict, Union, List, Tuple, NamedTuple, Type
 from abc import abstractmethod
 import numpy as np
 import jax
 import jax.numpy as jnp
 import jax.random as jrd
-from jaxtyping import Array, Float
+from jaxtyping import Array, Float, Int32
+from jax.typing import ArrayLike
+from jax.experimental.host_callback import call
 
-from jaxdp.algorithm import BaseAlgorithm
-from jaxdp.learning.sampler import Sampler, RolloutSample
+from jaxdp.learning.sampler import RolloutSample, SamplerState
+from jaxdp.mdp.mdp import MDP
 
 
-class BaseLearning(BaseAlgorithm):
+class TrainMetrics(NamedTuple):
+    avg_episode_rewards: Float[Array, "N"]
+    avg_episode_lengths: Float[Array, "N"]
+    std_episode_rewards: Float[Array, "N"]
+    std_episode_lengths: Float[Array, "N"]
+    max_value_diff: Float[Array, "N"]
+    avg_value_eval: Float[Array, "N"]
 
-    def __init__(self, state_size: int, action_size: int, seed: int) -> None:
-        super().__init__()
-        self.state_size = state_size
-        self.action_size = action_size
-        self.key, self.init_key = jrd.split(jrd.PRNGKey(seed), num=2)
-        self.value = self.initialize_value()
+    @staticmethod
+    def initialize(step_size: int) -> "TrainMetrics":
+        return TrainMetrics(*[jnp.full((step_size,), jnp.nan) for _ in range(6)])
 
-    @abstractmethod
-    def initialize_value(self) -> Float[Array, "... S"]:
-        pass
+    def write(self, index: int, values: Dict[str, float]) -> "TrainMetrics":
+        return TrainMetrics(
+            self.avg_episode_rewards.at[index].set(values["avg_episode_rewards"]),
+            self.avg_episode_lengths.at[index].set(values["avg_episode_lengths"]),
+            self.std_episode_rewards.at[index].set(values["std_episode_rewards"]),
+            self.std_episode_lengths.at[index].set(values["std_episode_lengths"]),
+            self.max_value_diff.at[index].set(values["max_value_diff"]),
+            self.avg_value_eval.at[index].set(values["avg_value_eval"]),
+        )
 
-    @abstractmethod
-    def policy(self) -> Float[Array, "A S"]:
-        pass
 
-    @abstractmethod
-    def value_update(self, rollout: RolloutSample, gamma: float) -> Float[Array, "... S"]:
-        pass
+QValueType: Type = Float[Array, "A S"]
+PolicyType: Type = Float[Array, "A S"]
+BatchKeys: Type = Int32[Array, "B 2"]
 
-    def run(self, sampler: Sampler, n_steps: int,  gamma: float
-            ) -> List[Dict[str, Float[Array, "..."]]]:
 
-        metrics = []
-        for _ in range(n_steps):
-            self.key, step_key = jrd.split(self.key, 2)
-            rollout_sample = sampler.rollout_sample(self.policy(), step_key)
-            next_value = self.value_update(rollout_sample, gamma)
-            metrics.append(self.gather_step_info(next_value, sampler, gamma))
-            self.value = next_value
+def train(sampler_state: SamplerState,
+          init_value: QValueType,
+          mdp,
+          key: ArrayLike,
+          eval_steps: int,
+          n_steps: int,
+          policy_fn: Callable[[QValueType, int], Float[Array, "A"]],
+          update_fn: Callable[[RolloutSample, QValueType], QValueType],
+          sample_fn: Callable[[MDP, SamplerState, PolicyType, BatchKeys], RolloutSample],
+          verbose: bool = True,
+          ) -> Tuple[TrainMetrics, QValueType]:
 
-        return metrics
+    metrics = TrainMetrics.initialize(n_steps // eval_steps)
+    value = init_value
 
-    def gather_step_info(self,
-                         next_value: Float[Array, "... S"],
-                         sampler: Sampler,
-                         gamma: float
-                         ) -> Dict[str, Float[Array, "..."]]:
-        return {
-            "episode_reward": sampler.recent_episode_rewards,
-            "episode_length": sampler.recent_episode_lengths,
-            "value_delta": (self.value - next_value).max(),
-            "value": self.value,
-        }
+    def print_log(data):
+        if verbose:
+            print(f"Progress: {data[0]:5d}, Avg episode reward: {data[1]:.2f}")
 
-    def metrics(self,
-                history: List[Dict[str, Float[Array, "..."]]]
-                ) -> Dict[str, Union[float, int, bool]]:
-        return {
-            "value_delta": [(next_info["value"] - prev_info["value"]).max().item()
-                            for prev_info, next_info in zip(history[:-1], history[1:])] + [None],
-            "mean_episode_reward": [np.mean(step_info["episode_reward"]) for step_info in history],
-            "mean_episode_length": [np.mean(step_info["episode_length"]) for step_info in history],
-        }
+    def log_fn(sampler_state, metrics, value_norm, index):
+        metrics = metrics.write(
+            index // eval_steps,
+            {"avg_episode_rewards": jnp.nanmean(sampler_state.episode_reward_queue),
+                "avg_episode_lengths": jnp.nanmean(sampler_state.episode_length_queue),
+                "std_episode_rewards": jnp.nanstd(sampler_state.episode_reward_queue),
+                "std_episode_lengths": jnp.nanstd(sampler_state.episode_length_queue),
+                "max_value_diff": value_norm,
+                "avg_value_eval": jnp.nan})
+        call(print_log,
+             (index + 1, jnp.nanmean(sampler_state.episode_reward_queue)))
+        sampler_state = sampler_state.refresh_queues()
+        return sampler_state, metrics
+
+    def step_fn(index, step_data):
+        metrics, sampler_state, value, key = step_data
+
+        key, step_key = jrd.split(key, 2)
+        step_keys = jrd.split(step_key, sampler_state.last_state.shape[0])
+        policy = policy_fn(value, index)
+        rollout_sample, sampler_state = sample_fn(mdp, sampler_state, policy, step_keys)
+        next_value = update_fn(rollout_sample, value)
+        value_norm = jnp.max(jnp.abs(next_value - value))
+
+        sampler_state, metrics, = jax.lax.cond(
+            (index % eval_steps) == (eval_steps - 1),
+            log_fn,
+            lambda sampler_state, metrics, *_: (sampler_state, metrics),
+            sampler_state, metrics, value_norm, index)
+
+        value = next_value
+        return metrics, sampler_state, value, key
+
+    metrics, sampler_state, value, key = jax.lax.fori_loop(
+        0, n_steps, step_fn, (metrics, sampler_state, value, key))
+
+    return metrics, value
+
+
+def evaluate_value():
+    raise NotImplementedError
--- jaxdp/learning/q_learning.py
+++ jaxdp/learning/q_learning.py
@@ -1,55 +1,37 @@
-from typing import Dict, Union, List, NamedTuple
-from abc import abstractmethod
+from typing import Dict, Union, List, NamedTuple, Tuple
+from functools import partial
 import jax
 import jax.numpy as jnp
 import jax.random as jrd
 from jaxtyping import Float, Array
+from jax.typing import ArrayLike
 
-import jaxdp
-from jaxdp.learning.learning import BaseLearning
-from jaxdp.learning.sampler import RolloutSample
+from jaxdp.learning.sampler import RolloutSample, StepSample
 
 
-def q_update(state, next_state, action, reward, terminal, timeout, value, gamma):
-    one_hot_value = jnp.einsum("s,a->as", state, action)
+def q_step(sample: StepSample,
+           value: Float[Array, "A S"],
+           gamma: float
+           ) -> Float[Array, "A S"]:
+    one_hot_value = jnp.einsum("s,a->as", sample.state, sample.action)
     return one_hot_value * (
-        gamma * jnp.max(jnp.einsum("x,ux,->u", next_state, value, (1 - terminal)), axis=0) +
-        reward -
-        jnp.einsum("s,a,as->", state, action, value))
+        gamma * jnp.max(jnp.einsum("x,ux,->u", sample.next_state,
+                        value, (1 - sample.terminal)), axis=0)
+        + sample.reward - jnp.einsum("s,a,as->", sample.state, sample.action, value))
 
 
-class QLearning(BaseLearning):
+def q_learning_update(rollout: RolloutSample,
+                      value: Float[Array, "A S"],
+                      gamma: float,
+                      alpha: float,
+                      ) -> Float[Array, "A S"]:
+    batch_q_update = jax.vmap(
+        jax.vmap(
+            q_step,
+                (0, None, None)),
+        (0, None, None))
 
-    def __init__(self,
-                 state_size: int,
-                 action_size: int,
-                 seed: int,
-                 alpha: float = 0.01,
-                 epsilon: float = 0.1,
-                 ) -> None:
-        super().__init__(state_size, action_size, seed)
-        self.alpha = alpha
-        self.epsilon = epsilon
-        self.batch_q_update = jax.jit(
-            jax.vmap(
-                jax.vmap(
-                    q_update,
-                    (0, 0, 0, 0, 0, 0, None, None)),
-                (0, 0, 0, 0, 0, 0, None, None))
-        )
-
-    def initialize_value(self) -> Float[Array, "... S"]:
-        return jrd.uniform(self.init_key, shape=(self.action_size, self.state_size))
-
-    @abstractmethod
-    def policy(self) -> Float[Array, "A S"]:
-        return jaxdp.e_greedy_policy(self.value, self.epsilon)
-
-    @abstractmethod
-    def value_update(self, rollout: RolloutSample, gamma: float) -> Float[Array, "... S"]:
-        target_values = self.batch_q_update(rollout.state, rollout.next_state, rollout.action,
-                                            rollout.reward, rollout.terminal, rollout.timeout,
-                                            self.value, gamma)
-        count = jnp.clip(jnp.einsum(
-            "bts,bta->as", rollout.state, rollout.action), 1.0, None)
-        return self.value + target_values.sum((0, 1)) / count * self.alpha
+    target_values = batch_q_update(rollout, value, gamma)
+    count = jnp.clip(jnp.einsum(
+        "bts,bta->as", rollout.state, rollout.action), 1.0, None)
+    return value + target_values.sum((0, 1)) / count * alpha
