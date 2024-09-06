0800506753d4d7e19a75b6e69b49a6dba8dfb907
TolgaOk <tolgaokk@gmail.com>
2024-09-06 13:22:02 +0200

    Update Sync learning update

--- jaxdp/learning/runner.py
+++ jaxdp/learning/runner.py
@@ -13,83 +13,95 @@ import jaxdp
 from jaxdp.utils import register_as
 
 
-class ExpDecay(NamedTuple):
-    temperature: float = 1.0
-    offset: int = 1
-
-    def decay_fn(self) -> Callable[[int], float]:
-        def decay(step: int):
-            return (1 / (step / self.temperature + self.offset))
-        return decay
-
-
-class TrainMetrics(NamedTuple):
-    avg_episode_rewards: F["N"]
-    avg_episode_lengths: F["N"]
-    std_episode_rewards: F["N"]
-    std_episode_lengths: F["N"]
-    max_value_diff: F["N"]
-    avg_value_eval: F["N"]
-
-    @staticmethod
-    def initialize(step_size: int) -> "TrainMetrics":
-        return TrainMetrics(*[jnp.full((step_size,), jnp.nan) for _ in range(6)])
-
-    def write(self, index: int, values: Dict[str, float]) -> "TrainMetrics":
-        self_dict = self._asdict()
-        return TrainMetrics(
-            *[self_dict[name].at[index].set(value)
-              for name, value in values.items()]
-        )
+class AsyncTrainMetrics(NamedTuple):
+    mean_behavior_reward: F["N"]
+    mean_behavior_length: F["N"]
+    std_behavior_reward: F["N"]
+    std_behavior_length: F["N"]
+    bellman_error: F["N"]
+    l_inf_value_norm: F["N"]
+    policy_evaluation: F["N"]
+    value_error: F["N"]
 
 
 class SyncTrainMetrics(NamedTuple):
-    expected_policy_eval: F["N"]
-    max_value_diff: F["N"]
+    policy_evaluation: F["N"]
+    l_inf_value_norm: F["N"]
     bellman_error: F["N"]
     expected_value: F["N"]
     value_error: F["N"]
 
-    @staticmethod
-    def initialize(step_size: int) -> "SyncTrainMetrics":
-        return SyncTrainMetrics(*[jnp.full((step_size,), jnp.nan)
-                                  for _ in range(5)])
+
+def initialize_metrics(metric_cls: Union[Type[AsyncTrainMetrics],
+                                         Type[SyncTrainMetrics]],
+                       size: int
+                       ) -> "AsyncTrainMetrics":
+    return metric_cls(*[jnp.full((size,), jnp.nan) for _ in metric_cls._fields])
 
 
 BatchKeys: Type = NewType("BatchKeys", I["B 2"])
 
 
+def eval_policy(mdp: MDP, policy: PiType, gamma: float) -> float:
+    return (jaxdp.policy_evaluation.v(mdp, policy, gamma) * mdp.initial).sum()
+
+
 @register_as("async")
 def train(sampler_state: SamplerState,
           init_value: QType,
-          mdp,
+          mdp: MDP,
           key: KeyType,
-          eval_steps: int,
+          learner_state: Any,
+          value_star: QType,
+          eval_period: int,
           n_steps: int,
-          policy_fn: Callable[[QType, int], F["A"]],
-          update_fn: Callable[[RolloutSample, QType], QType],
+          gamma: float,
+          behavior_policy_fn: Callable[[QType, int], PiType],
+          target_policy_fn: Callable[[QType, int], PiType],
+          update_fn: Callable,
           sample_fn: Callable[[MDP, SamplerState, PiType, BatchKeys], RolloutSample],
           verbose: bool = True,
-          ) -> Tuple[TrainMetrics, QType]:
+          ) -> Tuple[AsyncTrainMetrics, QType, SamplerState, Any]:
 
-    metrics = TrainMetrics.initialize(n_steps // eval_steps)
+    metrics = initialize_metrics(AsyncTrainMetrics, n_steps // eval_period)
     value = init_value
 
-    def print_log(data):
-        print(f"Progress: {data[0]:5d}, Avg episode reward: {data[1]:.2f}")
+    def _print_fn(print_data):
+        index, metrics = print_data
+        print("=" * 50)
+        print(f"{'Training Metrics - Iteration':^50} {index}")
+        print("")
+        for name in metrics._fields:
+            value = getattr(metrics, name)[index].item()
+            formatted_name = name.replace('_', ' ').title()
+            print(f"{formatted_name:<30} : {value:>15.4f}")
 
-    def log_fn(sampler_state, metrics, value_norm, index):
-        metrics = metrics.write(
-            index // eval_steps,
-            {"avg_episode_rewards": jnp.nanmean(sampler_state.episode_reward_queue),
-             "avg_episode_lengths": jnp.nanmean(sampler_state.episode_length_queue),
-             "std_episode_rewards": jnp.nanstd(sampler_state.episode_reward_queue),
-             "std_episode_lengths": jnp.nanstd(sampler_state.episode_length_queue),
-             "max_value_diff": value_norm,
-             "avg_value_eval": jnp.nan})
+    def _log_fn(sampler_state, metrics, step_data):
+        index, value, next_value = step_data
+        target_policy = target_policy_fn(value, index)
+
+        sampled_rewards = sampler_state.episode_reward_queue
+        sampled_lengths = sampler_state.episode_reward_queue
+
+        bellman_error = jnp.abs(jaxdp.bellman_operator.q(
+            mdp, target_policy, value, gamma) - value).max()
+        l_inf_value_norm = jnp.abs(next_value - value).max()
+        policy_evaluation = eval_policy(mdp, target_policy, gamma)
+        value_error = jnp.linalg.norm(value - value_star, ord=2)
+
+        metrics = AsyncTrainMetrics(
+            metrics.mean_behavior_reward.at[index].set(jnp.nanmean(sampled_rewards)),
+            metrics.mean_behavior_length.at[index].set(jnp.nanmean(sampled_lengths)),
+            metrics.std_behavior_reward.at[index].set(jnp.nanstd(sampled_rewards)),
+            metrics.std_behavior_length.at[index].set(jnp.nanstd(sampled_lengths)),
+            metrics.bellman_error.at[index].set(bellman_error),
+            metrics.l_inf_value_norm.at[index].set(l_inf_value_norm),
+            metrics.policy_evaluation.at[index].set(policy_evaluation),
+            metrics.value_error.at[index].set(value_error),
+        )
+
         if verbose:
-            call(print_log,
-                 (index + 1, jnp.nanmean(sampler_state.episode_reward_queue)))
+            call(_print_fn, (index + 1, metrics))
         sampler_state = sampler_state.refresh_queues()
         return sampler_state, metrics
 
@@ -98,16 +110,15 @@ def train(sampler_state: SamplerState,
 
         key, step_key = jrd.split(key, 2)
         step_keys = jrd.split(step_key, sampler_state.last_state.shape[0])
-        policy = policy_fn(value, index)
-        rollout_sample, sampler_state = sample_fn(mdp, sampler_state, policy, step_keys)
-        next_value = update_fn(rollout_sample, value)
-        value_norm = jnp.max(jnp.abs(next_value - value))
+        behavior_policy = behavior_policy_fn(value, index)
+        rollout_sample, sampler_state = sample_fn(mdp, sampler_state, behavior_policy, step_keys)
+        next_value = update_fn(index, rollout_sample, value, learner_state, gamma)
 
         sampler_state, metrics, = jax.lax.cond(
-            (index % eval_steps) == (eval_steps - 1),
-            log_fn,
+            (index % eval_period) == (eval_period - 1),
+            _log_fn,
             lambda sampler_state, metrics, *_: (sampler_state, metrics),
-            sampler_state, metrics, value_norm, index)
+            sampler_state, metrics, (index, value, next_value))
 
         value = next_value
         return metrics, sampler_state, value, key
@@ -118,29 +129,21 @@ def train(sampler_state: SamplerState,
     return metrics, value
 
 
-@register_as("async")
-def evaluate():
-    raise NotImplementedError
-
-
 @register_as("sync")
 def train(init_value: QType,
           mdp: MDP,
-          value_star: QType,
           key: KeyType,
           learner_state: Any,
+          value_star: QType,
           n_steps: int,
           eval_period: int,
           gamma: float,
-          policy_fn: Callable,
+          target_policy_fn: Callable,
           update_fn: Callable,
           ) -> Any:
 
-    metrics = SyncTrainMetrics.initialize(n_steps)
+    metrics = initialize_metrics(SyncTrainMetrics, n_steps)
     value = init_value
-
-    def _eval_policy(policy):
-        return (jaxdp.policy_evaluation.q(mdp, policy, gamma) * mdp.initial).sum()
 
     def _step_fn(index, _step_data):
         metrics, value, learner_state, key = _step_data
@@ -148,27 +151,24 @@ def train(init_value: QType,
 
         reward, next_state, terminal = jaxdp.sync_sample(mdp, step_key)
         sample = SyncSample(next_state, reward, terminal)
+        next_value, learner_state = update_fn(index, sample, value, learner_state, gamma)
 
-        _next_value, learner_state = update_fn(index, sample, value, learner_state, gamma)
-        # next_value = jnp.einsum("as,s->as", _next_value, 1 - mdp.terminal)
-        next_value = _next_value
-
-        policy = policy_fn(next_value, index)
-        expected_policy_eval = jax.lax.cond(
+        target_policy = target_policy_fn(next_value, index)
+        policy_evaluation = jax.lax.cond(
             (index % eval_period) == (eval_period - 1),
-            _eval_policy,
-            lambda _: jnp.nan,
-            policy
+            eval_policy,
+            lambda *_: jnp.nan,
+            mdp, target_policy, gamma
         )
-        expected_value = jnp.einsum("as,as,s->", policy, value, mdp.initial)
-        max_value_diff = jnp.abs(next_value - value).max()
+        expected_value = jnp.einsum("as,as,s->", target_policy, value, mdp.initial)
+        l_inf_value_norm = jnp.abs(next_value - value).max()
         bellman_error = jnp.abs(jaxdp.bellman_operator.q(
-            mdp, policy, value, gamma) - value).max()
+            mdp, target_policy, value, gamma) - value).max()
         value_error = jnp.linalg.norm(value - value_star, ord=2)
 
         metrics = SyncTrainMetrics(
-            metrics.expected_policy_eval.at[index].set(expected_policy_eval),
-            metrics.max_value_diff.at[index].set(max_value_diff),
+            metrics.policy_evaluation.at[index].set(policy_evaluation),
+            metrics.l_inf_value_norm.at[index].set(l_inf_value_norm),
             metrics.bellman_error.at[index].set(bellman_error),
             metrics.expected_value.at[index].set(expected_value),
             metrics.value_error.at[index].set(value_error),
@@ -180,11 +180,6 @@ def train(init_value: QType,
         0, n_steps, _step_fn, (metrics, value, learner_state, key))
 
     return metrics, value, learner_state
-
-
-@register_as("sync")
-def evaluate():
-    raise NotImplementedError
 
 
 def no_learner_state(update_fn: Callable):
